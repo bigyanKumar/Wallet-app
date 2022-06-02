82c23e21f8689aa530fefd81f8a533df9ab9edc9
Bigyan Kumar Sandilya <bigyankumarnwd@gmail.com>
2022-06-02 21:35:45 +0530

    wallet_application will work fine

--- Wallet_Application/src/main/java/com/masai/WalletApplication.java
+++ Wallet_Application/src/main/java/com/masai/WalletApplication.java
@@ -1,14 +1,13 @@
-package com.example.demo;
+package com.masai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class WalletAppApplication {
+public class WalletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WalletAppApplication.class, args);
+		SpringApplication.run(WalletApplication.class, args);
 	}
 
 }
