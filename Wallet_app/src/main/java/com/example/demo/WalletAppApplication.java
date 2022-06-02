2eda9340ff520556b136e3b9aea95ea575216db9
Bigyan Kumar Sandilya <bigyankumarnwd@gmail.com>
2022-06-02 19:35:40 +0530

    strucher final

---
+++ Wallet_app/src/main/java/com/example/demo/WalletAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletAppApplication.class, args);
+	}
+
+}
