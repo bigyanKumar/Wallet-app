d92de53793817ec0e0eaee2b570f56f9d70eaf91
mahendrasingh-engineer <59507085+mahendrasingh-engineer@users.noreply.github.com>
2022-06-04 00:47:12 +0530

    committed by Mahendra Singh
    completed  Wallet entity class and dao interface

--- Wallet_Application/src/main/java/com/masai/entity/Wallet.java
+++ Wallet_Application/src/main/java/com/masai/entity/Wallet.java
@@ -30,4 +30,26 @@ public class Wallet {
 	private Integer id;
 	@Min(value=0, message="Balance should be greate than 0")
 	private Double balance;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	public Wallet(Integer id, @Min(value = 0, message = "Balance should be greate than 0") Double balance) {
+		super();
+		this.id = id;
+		this.balance = balance;
+	}
+	public Wallet() {
+		super();
+	}
+	
+	
 }
