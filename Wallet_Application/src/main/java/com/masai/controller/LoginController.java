68127930c1c4dd29fa59130c17ff3ed99d18c82a
Bigyan Kumar Sandilya <bigyankumarnwd@gmail.com>
2022-06-05 17:25:28 +0530

    revert

--- Wallet_Application/src/main/java/com/masai/controller/LoginController.java
+++ Wallet_Application/src/main/java/com/masai/controller/LoginController.java
@@ -1,33 +1,35 @@
-package com.masai.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LoginController {
-
-	@Autowired
-	private CustomerLogInImpl customerLogIn;
-	
-	
-	@PostMapping("/login")
-	public String lopInCustomer(@RequestParam("mobileNo") long mobileNo,@RequestParam("password") String password)
-	{
-		return  customerLogIn.logIntoAccount(mobileNo,password);
-	}
-	
-	 
-	@PatchMapping("logout")
-	public String logOutCustomer(@RequestParam(required =false) String key)
-	{
-		return customerLogIn.logOutFromAccount(key);
-	}
-}
-
-
-
+//package com.masai.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+////import org.springframework.stereotype.Service;
+//import org.springframework.web.bind.annotation.PatchMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.masai.service.impl.CustomerLogInImpl;
+//
+//@RestController
+//public class LoginController {
+//
+//	@Autowired
+//	private CustomerLogInImpl customerLogIn;
+//	
+//	
+//	@PostMapping("/login")
+//	public String lopInCustomer(@RequestParam("mobileNo") long mobileNo,@RequestParam("password") String password)
+//	{
+//		return  customerLogIn.logIntoAccount(mobileNo,password);
+//	}
+//	
+//	 
+//	@PatchMapping("logout")
+//	public String logOutCustomer(@RequestParam(required =false) String key)
+//	{
+//		return customerLogIn.logOutFromAccount(key);
+//	}
+//}
+//
+//
+//
