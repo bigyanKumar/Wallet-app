1079201586991bbe73b667c2c5c3a3c691266c31
Durgesh Soni <durgeshsoni1082003@gmail.com>
2022-06-05 14:02:20 +0530

    bill payment done

--- Wallet_Application/src/main/java/com/masai/service/BillPaymentServicesImpl.java
+++ Wallet_Application/src/main/java/com/masai/service/BillPaymentServicesImpl.java
@@ -1,11 +1,14 @@
 package com.masai.service;
 
+import java.time.LocalDateTime;
+
 //import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.masai.entity.BillPayment;
+import com.masai.entity.Transaction;
 import com.masai.entity.Wallet;
 import com.masai.globalExceptionHandler.InsufficientAmountException;
 import com.masai.repository.BillPaymentDAO;
@@ -19,14 +22,32 @@ public class BillPaymentServicesImpl implements BillPaymentServices {
 	@Autowired
 	private BillPaymentDAO billpd;
 	
+	@Autowired
 	private WalletDao walletdao;
+	
+	@Autowired
+	private TransactionDAO transactionDAO;
 
 	@Override
 	public String payBill(double amount, String billType) {
 		
+		if(wallet.getBalance() <= amount) {
+			throw new InsufficientAmountException("Insufficient amount in wallet");
+		}
 		
+		wallet.setWalletBalance(wallet.getWalletBalance() - amount);
+		  Transaction tx = new Transaction();
+		    tx.setAmount(amount);
+		    tx.setDateTime(LocalDateTime.now());
+		    tx.setDescription(billType);
+		    tx.setTransactionType("Debit");
+		    
+		    
+			transactionDAO.save(myTransaction);
+			walletDAO.save(wallet);
+			
 		
-		return "Successfully done...";
+		return "Successfully done...  "  ;
 	}
 	
 	
