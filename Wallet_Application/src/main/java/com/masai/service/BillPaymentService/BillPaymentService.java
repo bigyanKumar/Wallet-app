f6b24faaceaf91ddb555b1a05b06d239f3417169
Bigyan Kumar Sandilya <97912572+bigyanKumar@users.noreply.github.com>
2022-06-06 16:25:44 +0530

    NO NEED

--- Wallet_Application/src/main/java/com/masai/service/BillPaymentService/BillPaymentService.java
+++
@@ -1,9 +0,0 @@
-//package com.masai.service.BillPaymentService;
-//
-//import com.masai.entity.BillPayment;
-//
-//public interface BillPaymentService {
-//	
-//	public BillPayment addBillPayment(BillPayment billPayment);
-//
-//}
